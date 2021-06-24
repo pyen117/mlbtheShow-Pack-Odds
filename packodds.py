0297bac1d5f82802828d28e00551a8e34854a567
pyen117 <86397485+pyen117@users.noreply.github.com>
2021-06-24 01:50:19 -0400

    Update packodds.py

--- packodds.py
+++ packodds.py
@@ -8,7 +8,19 @@ requests.packages.urllib3.disable_warnings()
 
 
 def get_cards(csv_file):
-    headers = {'your headers go here}
+    headers = {
+    'authority': 'mlb21.theshow.com',
+    'sec-ch-ua': '^\\^',
+    'accept': 'text/html, application/xhtml+xml',
+    'turbolinks-referrer': 'https://mlb21.theshow.com/packs/open_pack_history^#',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://mlb21.theshow.com/packs/open_pack_history',
+    'accept-language': 'en-US,en;q=0.9',
+    'cookie': 'tsn_hide_ribbon=1-1619213959; tsn_last_seen_roster_id=2; tsn_last_url=--CCFFYHBDZE06TDQk68hJ_-IxQblpnnntq3LUcuMDuwHG_gcSJghq6kTM9xZE0u70z9piO-A7ikVh1BRqA6bw^%^3D^%^3D; _tsn_session=e1ce8aa303becbab37d8f6159ad783b9; tsn_token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NTUwOTg2LCJ1c2VybmFtZSI6IllFTiBXSU5TX1hCTCIsInBpY3R1cmUiOiJodHRwczovL3RoZXNob3duYXRpb24tcHJvZHVjdGlvbi5zMy5hbWF6b25hd3MuY29tL2ZvcnVtX2ljb25zL2ljb25fYW5pbWFsc19kb2cucG5nIiwiZ3JvdXBzIjpbXX0.hQKyAbFMmKtnVBkloyLXFSd6j_4AryGPP0ubx3R8tno',}
 
     
     try:
